4a6c828423d21a3aaa3eff2897add625ee2f3b09
EvanWallSaintPaul <evanw.mn@gmail.com>
2018-04-10 17:50:57 -0500

    cli working

--- src/java/network/Driver.java
+++ src/java/network/Driver.java
@@ -1,19 +1,23 @@
 package network;
-import java.util.*;
-import org.apache.commons.cli.*;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 public class Driver {
 	//------------- public map for all nodes on network----
 	public static int CLIENTPORT = 9875;
     public static int SERVERPORT = 9876;
     public static String IPADDRESS = "localhost";
-    public static int INTERFERENCE_PERCENTAGE = 0;
-    public static int PACKET_SIZE = 500;
+    public static int INTERFERENCE_PERCENTAGE = 20;
+    public static int PACKET_SIZE = 50;
     public static int WINDOW_SIZE = 5;
     public static int TIMEOUT_INTERVAL = 2000;
     @SuppressWarnings("unused")
     public static void main(String args[]) {
+
     		//  command line flags code
 	    	Options options = new Options();
 	    	options.addOption("boot", true, "boot client or server");
@@ -26,20 +30,36 @@ public class Driver {
 
 	    	CommandLineParser parser = new DefaultParser();
 	    	CommandLine cmd = null;
-			try {
-				cmd = parser.parse( options, args);
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	   	PACKET_SIZE = Integer.parseInt(cmd.getOptionValue("s"));
-	    	TIMEOUT_INTERVAL = Integer.parseInt(cmd.getOptionValue("t"));
-	    	WINDOW_SIZE = Integer.parseInt(cmd.getOptionValue("w"));
-	    	INTERFERENCE_PERCENTAGE = (int) Double.parseDouble((cmd.getOptionValue("d"))) * 100;
-	    IPADDRESS = cmd.getOptionValue("ip");
-	    
+		try {
+			cmd = parser.parse( options, args);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (cmd.hasOption("s")) {
+	        PACKET_SIZE = Integer.parseInt(cmd.getOptionValue("s"));
+	        System.out.println("Packet size: " + PACKET_SIZE);
+
+		}
+
+		if (cmd.hasOption("t")) {
+	        TIMEOUT_INTERVAL = Integer.parseInt(cmd.getOptionValue("t"));
+		}
+		
+		if (cmd.hasOption("w")) {
+	        WINDOW_SIZE = Integer.parseInt(cmd.getOptionValue("w"));
+		}
+
+		if (cmd.hasOption("d")) {
+	        INTERFERENCE_PERCENTAGE = (int) (Double.parseDouble(cmd.getOptionValue("d")) * 100);
+		}
+
+		if (cmd.hasOption("ip")) {
+	        IPADDRESS = cmd.getOptionValue("ip");
+		}
+		
 	    	//  end of command line flags code
-	    	
+
         /* Client Constructor parameters:
          * int packetSize -- the size of each packet sent, 1-500.
          * int timeOut -- the duration before the client socket times out, in milliseconds
@@ -54,17 +74,23 @@ public class Driver {
          * int interference -- see client
          * InetAddress iPAddress -- the iPAddress to set this server to.
          * int port -- see client
-         */
-	    	if (cmd.getOptionValue("boot").equals("client")) {
-	    		   CLIENTPORT = Integer.parseInt(cmd.getOptionValue("port"));
+        */
+		if (cmd.hasOption("boot")) {
+		 	if (cmd.getOptionValue("boot").equals("client")) {
+		 		if (cmd.hasOption("port")) {
+		    		   CLIENTPORT = Integer.parseInt(cmd.getOptionValue("port"));
+		 		}
 	    	       Client client = new Client(PACKET_SIZE, TIMEOUT_INTERVAL, WINDOW_SIZE,Driver.CLIENTPORT, INTERFERENCE_PERCENTAGE, 1000);
-
-	    	}else if (cmd.getOptionValue("boot").equals("server")) {
-	    	       SERVERPORT = Integer.parseInt(cmd.getOptionValue("port"));
+		 	}else if (cmd.getOptionValue("boot").equals("server")) {
+		 		if (cmd.hasOption("port")) {
+		    	       SERVERPORT = Integer.parseInt(cmd.getOptionValue("port"));
+		 		}
 	    	       Server server = new Server(WINDOW_SIZE ,INTERFERENCE_PERCENTAGE ,IPADDRESS,Driver.SERVERPORT);
-	    	}else {
-	    	       Server server = new Server(WINDOW_SIZE ,INTERFERENCE_PERCENTAGE ,IPADDRESS,Driver.SERVERPORT);
-	    	       Client client = new Client(PACKET_SIZE, TIMEOUT_INTERVAL, WINDOW_SIZE,Driver.CLIENTPORT, INTERFERENCE_PERCENTAGE, 1000);
+		 	}	
+		}
+		else {
+          Server server = new Server(WINDOW_SIZE ,INTERFERENCE_PERCENTAGE ,IPADDRESS,Driver.SERVERPORT);
+          Client client = new Client(PACKET_SIZE, TIMEOUT_INTERVAL, WINDOW_SIZE,Driver.CLIENTPORT, INTERFERENCE_PERCENTAGE, 1000);
 	    	}
     }
 }
