cca185b0298426adea8f97033f6f6036446146db
EvanWallSaintPaul <evanw.mn@gmail.com>
2018-04-09 23:15:39 -0500

    for the sake of gui requirements

--- src/java/network/Driver.java
+++ src/java/network/Driver.java
@@ -1,21 +1,45 @@
 package network;
 import java.util.*;
+import org.apache.commons.cli.*;
+
 
 public class Driver {
 	//------------- public map for all nodes on network----
-	public final static int CLIENTPORT = 9875;
-    public final static int SERVERPORT = 9876;
-    //--------------end of network map --------------------
-
-    static Scanner userInput = new Scanner(System.in);
-    static String cliOrServ;
-    public static int INTERFERENCE_PERCENTAGE = 10;
-
+	public static int CLIENTPORT = 9875;
+    public static int SERVERPORT = 9876;
+    public static String IPADDRESS = "localhost";
+    public static int INTERFERENCE_PERCENTAGE = 0;
+    public static int PACKET_SIZE = 500;
+    public static int WINDOW_SIZE = 5;
+    public static int TIMEOUT_INTERVAL = 2000;
     @SuppressWarnings("unused")
     public static void main(String args[]) {
-        // clientOrServerMethod();
-        // userEnterInterferencePercentage();
+    		//  command line flags code
+	    	Options options = new Options();
+	    	options.addOption("boot", true, "boot client or server");
+	    	options.addOption("s", true, "packet size");
+	    	options.addOption("t", true, "timeout interval");
+	    	options.addOption("w", true, "window size");
+	    	options.addOption("d", true, "percentage of datagrams to corrupt, delay, or drop");
+	    	options.addOption("ip", true, "ip address");
+	    	options.addOption("port", true, "port");
 
+	    	CommandLineParser parser = new DefaultParser();
+	    	CommandLine cmd = null;
+			try {
+				cmd = parser.parse( options, args);
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	   	PACKET_SIZE = Integer.parseInt(cmd.getOptionValue("s"));
+	    	TIMEOUT_INTERVAL = Integer.parseInt(cmd.getOptionValue("t"));
+	    	WINDOW_SIZE = Integer.parseInt(cmd.getOptionValue("w"));
+	    	INTERFERENCE_PERCENTAGE = (int) Double.parseDouble((cmd.getOptionValue("d"))) * 100;
+	    IPADDRESS = cmd.getOptionValue("ip");
+	    
+	    	//  end of command line flags code
+	    	
         /* Client Constructor parameters:
          * int packetSize -- the size of each packet sent, 1-500.
          * int timeOut -- the duration before the client socket times out, in milliseconds
@@ -24,44 +48,23 @@ public class Driver {
          * int interference -- the percentage of packets that will be dropped or corrupted
          * int delay -- the delay time in milliseconds of the simulation
          */
-       Client client = new Client(50,2000,5,Driver.CLIENTPORT, 20, 1000);
+        /*
+         * Server Constructor parameters:
+         * int windowSize -- see client
+         * int interference -- see client
+         * InetAddress iPAddress -- the iPAddress to set this server to.
+         * int port -- see client
+         */
+	    	if (cmd.getOptionValue("boot").equals("client")) {
+	    		   CLIENTPORT = Integer.parseInt(cmd.getOptionValue("port"));
+	    	       Client client = new Client(PACKET_SIZE, TIMEOUT_INTERVAL, WINDOW_SIZE,Driver.CLIENTPORT, INTERFERENCE_PERCENTAGE, 1000);
 
-       /*
-        * Server Constructor parameters:
-        * int windowSize -- see client
-        * int interference -- see client
-        * InetAddress iPAddress -- the iPAddress to set this server to.
-        * int port -- see client
-        */
-
-       Server server = new Server(5,20,"localhost",Driver.SERVERPORT);
-
-       // clientOrServerMethod();
-    		//userEnterInterferencePercentage();
-    		//Proxy proxy = new Proxy();
-    }
-    private static void userEnterInterferencePercentage() {
-	    	do {
-	    		System.out.println("Enter any int 0-100 percentage of packets to be intefered with: ");
-	    		INTERFERENCE_PERCENTAGE = userInput.nextInt();
-	    	}while(!(INTERFERENCE_PERCENTAGE >= 0) || !(INTERFERENCE_PERCENTAGE <= 100)) ;
-	}
-
-    private static void clientOrServerMethod() {
-        System.out.println("Enter 'client' to run client instance, or 'server' to run server instance. ");
-        do {
-            cliOrServ = userInput.nextLine().toLowerCase();
-            switch (cliOrServ) {
-                case "client":
-                   // Client client = new Client();
-                    return;
-                case "server":
-                    //Server server = new Server();
-                    return;
-                default:
-                    System.out.println("Invalid entry. \nEnter 'client' to run client instance, or 'server' to run server instance. ");
-                    break;
-            }
-        }while(!cliOrServ.equals("client") || !cliOrServ.equals("server")) ;
+	    	}else if (cmd.getOptionValue("boot").equals("server")) {
+	    	       SERVERPORT = Integer.parseInt(cmd.getOptionValue("port"));
+	    	       Server server = new Server(WINDOW_SIZE ,INTERFERENCE_PERCENTAGE ,IPADDRESS,Driver.SERVERPORT);
+	    	}else {
+	    	       Server server = new Server(WINDOW_SIZE ,INTERFERENCE_PERCENTAGE ,IPADDRESS,Driver.SERVERPORT);
+	    	       Client client = new Client(PACKET_SIZE, TIMEOUT_INTERVAL, WINDOW_SIZE,Driver.CLIENTPORT, INTERFERENCE_PERCENTAGE, 1000);
+	    	}
     }
 }
